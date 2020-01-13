b095f22badb160c70745c3705baeec8f5c05a002
m-mohsin-iqbal <m.mohsin.iqbal@outlook.com>
2020-01-13 14:45:54 +0500

    First Commit

---
+++ Attendance/app/src/main/java/com/attendance/Attendance.java
@@ -0,0 +1,34 @@
+package com.attendance;
+
+import android.app.Application;
+
+import com.androidnetworking.AndroidNetworking;
+import com.attendance.misc.utils.Extras;
+
+
+
+
+public class Attendance extends Application {
+
+    private static Attendance istance;
+    private Extras pref;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        istance = this;
+        AndroidNetworking.initialize(this);
+        pref = new Extras(this);
+        pref.setrollNo(36);
+    }
+
+    /**
+     * Instance of this class
+     * @return
+     */
+    public static Attendance getInstance() {
+        return istance;
+    }
+
+}
+
